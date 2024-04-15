d1a49f7b7d18c168f8a7e04614cc60641a003d27
hulin <piaohulin2007@gmail.com>
2024-04-15 21:40:37 +1000

    Added prettier as a code formater for every git commit:
    Config prettier as code formater
    Added .prettierignore file to ignore format checking(including * mark, it play as a swith of turn on/off the format checking)
    Added prettier-format.sh script to handle format fixing and git re-add the fixed files.
    Updated prettier-format.sh script to handle an empty git commit.
    Steup Husky and Lint-staged in order to trigger prettier every git commit command.
    Update README.md

--- prettier/prettier-format.sh
+++ prettier/prettier-format.sh
@@ -6,31 +6,36 @@ echo "Checking code format based on .prettierrc rules..."
 FILES_TO_FORMAT=$(git diff --cached --name-only --diff-filter=d | grep -E "^(src|test)/.*\.(ts|tsx)$")
 
 if [[ -n "$FILES_TO_FORMAT" ]]; then
-    echo "Code format issues detected:"
-    echo "Files that need formatting:"
-    echo "#########################"
-    echo "$FILES_TO_FORMAT"
-    echo "#########################"
+    NEEDS_FORMATTING=""
 
     for file in $FILES_TO_FORMAT; do
-        # Temporarily save the current state of the file for comparison after formatting
-        git diff --cached --quiet -- "$file"
-        PRE_FORMAT_HASH=$(git hash-object "$file")
-
-        echo "Formatting file: $file"
-        yarn prettier --write --config ./prettier/.prettierrc "$file"
-
-         # Compare the state of the file after formatting to determine if changes occurred
-        POST_FORMAT_HASH=$(git hash-object "$file")
-        if [ "$PRE_FORMAT_HASH" != "$POST_FORMAT_HASH" ]; then
-            git add "$file"
-            echo "$file has been reformatted and re-staged."
-        else
-            echo "$file was checked but did not require reformatting."
+        # Check the current format of the file using the format:check script
+        yarn format:check "$file"
+        if [ $? -ne 0 ]; then
+            # File needs formatting
+            NEEDS_FORMATTING+="$file "
         fi
     done
 
-    echo "Format issues have been checked and necessary files re-staged. Proceeding with commit."
+    if [[ -n "$NEEDS_FORMATTING" ]]; then
+        echo "To be committed files that need formatting:"
+        echo "#########################"
+        echo "$NEEDS_FORMATTING"
+        echo "#########################"
+
+        # Format files that need formatting
+        for file in $NEEDS_FORMATTING; do
+            echo "Formatting file: $file"
+            yarn format "$file"
+            # Add formatted file to staging
+            git add "$file"
+            echo "$file has been reformatted and re-staged."
+        done
+
+        echo "All format issues have been corrected and necessary files re-staged."
+    else
+        echo "All files are properly formatted."
+    fi
 else
-    echo "All files are properly formatted."
+    echo "No .ts or .tsx files in 'src' or 'test' directories need checking."
 fi
